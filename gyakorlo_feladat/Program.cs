a625a3fd8ad6e478a7dd00f6c773f2c7b2fef4c6
Peter <40338605+LaptopKing@users.noreply.github.com>
2020-11-03 19:59:26 +0100

    Update Program.cs

--- gyakorlo_feladat/Program.cs
+++ gyakorlo_feladat/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
@@ -8,6 +8,7 @@ namespace gyakorlo_feladat
     class Program
     {
         static List<double> ertekek = new List<double>();
+        static double atlag = 0;
         static void Main(string[] args)
         {
             StreamReader streamReader = new StreamReader("NO2.txt");
@@ -26,6 +27,15 @@ namespace gyakorlo_feladat
 
             // Third task
             Harmadik_feladat();
+
+            // Fourth task
+            Negyedik_feladat();
+
+            // Fifth task
+            Otodik_feladat();
+
+            // Sixth task
+            Hatodik_feladat();
         }
 
         // Displaying the values to command line with a line break after every fourth value
@@ -87,13 +97,57 @@ namespace gyakorlo_feladat
             }
             Console.WriteLine("Összesen " + ertek_50 + " darab 50 feletti mérési érték van!\n");
         }
+
         // Displaying the lowest and the largest number in the dataset
         static void Negyedik_feladat()
         {
             int min = ertekek[0];
             int max = ertekek[0];
 
-            for (int i = 0; )
+            for (int i = 0; ertekek.Count(); i++)
+            {
+                if (min > ertekek[i])
+                {
+                    min = ertekek[i];
+                }
+                if (max < ertekek[i])
+                {
+                    max = ertekek[i];
+                }
+            }
+
+            Console.WriteLine("Legkisebb szám: " + min + "\nLegnagyobb szám: " + max + "\n");
+        }
+
+        // Displaying the dataset's values average
+        static void Otodik_feladat()
+        {
+            double ossz = 0;
+
+            for (int i = 0; ertekek.Count(); i++)
+            {
+                ossz += ertekek[i];
+            }
+
+            atlag = ossz / ertekek.Count();
+
+            Console.WriteLine("Értékek átlaga: " + atlag + "\n");
+        }
+
+        // Displaying the values that are bigger than the average
+        static void Hatodik_feladat()
+        {
+            int darab = 0;
+
+            for (int i = 0; ertekek.Count(); i++)
+            {
+                if (ertekek[i] > atlag)
+                {
+                    darab++;
+                }
+            }
+
+            Console.WriteLine("Átlag értéknél nagyobb számok darabszáma: " + darab);
         }
     }
 }
